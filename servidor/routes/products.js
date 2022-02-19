aa36102bae7f797b02d05336bbb500f6e1605884
Jhonny Mendoza <jhomen11@gmail.com>
2022-02-19 00:46:16 -0300

    Routes get,put, delete

--- servidor/routes/products.js
+++ servidor/routes/products.js
@@ -2,9 +2,6 @@ const express = require('express')
 const router = express.Router()
 const Product = require('../database/models/ProductsModel')
 
-router.get('/', (req, res)=>{
-    res.send("Hola")
-})
 
 //POST
 router.post('/', (req, res)=>{
@@ -12,13 +9,46 @@ router.post('/', (req, res)=>{
         name: req.body.name,
         price: req.body.price,
         description: req.body.description
-    }).then(post =>{
-        res.json(post)
+    }).then(product =>{
+        res.json(product)
     })
 })
 //GET
+router.get('/', (req, res)=>{
+    Product.findAll().then(products => {
+        res.json(products)
+    })
+})
+
 //GET/:id
+router.get('/:id', (req, res)=>{
+    Product.findByPk(req.params.id).then(product => {
+        res.json(product)
+    })
+})
 //PUT
+router.put('/:id', (req, res) => {
+    Product.update({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description
+    }, {
+        where:{
+            id: req.params.id
+        }
+    }).then(result =>{
+        res.json(result)
+    })
+})
 //DELETE
+router.delete('/:id', (req, res) => {
+    Product.destroy({
+        where:{
+            id: req.params.id
+        }
+    }).then(result => {
+        res.json(result)
+    })
+})
 
 module.exports = router
